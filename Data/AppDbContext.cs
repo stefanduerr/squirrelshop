06696e8edf7f8b6a671f4b87e552c74e4c83163c
vlahunter <vlahunter@hotmail.com>
2024-12-11 11:44:18 +0100

    Added AppDbContext, EFCore and Npgsql

---
+++ Data/AppDbContext.cs
@@ -0,0 +1,10 @@
+namespace Squirrels.Data;
+using Microsoft.EntityFrameworkCore;
+
+public class AppDbContext : DbContext
+{
+    public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+    {
+
+    }
+}
